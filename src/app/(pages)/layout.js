3f0c3184e0e57fce3f04384ce898b099f48ae0a3
WalidurTanjim <mohammadtanjim91@gmail.com>
2025-10-17 16:14:06 +0600

    Link added

--- src/app/(pages)/layout.js
+++ src/app/(pages)/layout.js
@@ -1,9 +1,12 @@
 import "@/app/globals.css";
+import Navbar from "@/shared/Navbar/Navbar";
 
 const RootLayout = ({ children }) => {
   return (
     <html lang="en">
       <body>
+        <Navbar />
+
         <main>
           { children }
         </main>
