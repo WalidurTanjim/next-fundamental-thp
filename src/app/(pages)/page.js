12dcfce382f1b3dc45d74a842566299db0e08fb6
WalidurTanjim <mohammadtanjim92@gmail.com>
2025-11-08 18:23:45 +0600

    Another p tag added

--- src/app/(pages)/page.js
+++ src/app/(pages)/page.js
@@ -4,6 +4,7 @@ export default function Home() {
     <div className="home">
       <div className="container mx-auto w-full max-w-7xl px-2 sm:px-6 lg:px-8 py-14">
         <h1 className="text-center text-2xl text-slate-700 font-medium">Next Fundamental THP</h1>
+        <p>Another tag</p>
       </div>
     </div>
   );
